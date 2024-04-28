429d45de8663a847215fddd4f93b42cb0205a397
dylaniscool54 <106529128+dylaniscool54@users.noreply.github.com>
2024-04-28 14:04:11 +1000

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ app.get("/dreams", async (req, res) => {
       continue;
     }
 
-    if (amountin >= 99) {
+    if (amountin >= 100) {
       allrequesturls.push(a);
       a = "";
       amountin = 0;
